84c2bc985a8312ddd41ec2c1fce79ff32b230ee3
Otto Micheletti <michelettiotto@gmail.com>
2022-02-02 20:41:26 -0300

    Requisito 8

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,12 +1,16 @@
 const fetchProducts = async (arg) => {
   const url = `https://api.mercadolibre.com/sites/MLB/search?q=${arg}`;
   try {
-    const response = await fetch(url);
-    const respObj = await response.json();
-    const { results } = respObj;
-    return results;
+    if (!arg) {
+      throw new Error('You must provide an url');
+    } {
+      const response = await fetch(url);
+      const respObj = await response.json();
+      const { results } = respObj;
+      return results;
+    }
   } catch (error) {
-    return error;
+      return error;
   }
 };
 
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,31 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('1 - Verifica se fetchProducts é uma função.', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+
+  test('2 - Verifica a função fetchProducts com o argumento "computador" e testa se fetch foi chamada.', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  test('3 - Verifica se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador".', async () => {
+  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  await fetchProducts('computador');
+  expect(fetch).toHaveBeenCalledWith(url);
+  });
+
+  test('4 - Verifica se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo.', async () => {
+    const obj = await fetchProducts('computador');
+    expect(Object.keys(obj[0].seller)).toEqual(Object.keys(computadorSearch.results[0].seller));
+  });
+
+  test('5 - Verifica se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: You must provide an url.', () => {
+    // const obj = await fetchProducts();
+    expect( async () => {
+      return await fetchProducts();
+    }).toThrowError(new Error('You must provide an url'));
+  });
+ 
 });
